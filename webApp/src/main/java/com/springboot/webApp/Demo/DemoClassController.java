7f8f1391e44857f637accaa4f8ea20b5764b9f96
omjee247 <omjee247@gmail.com>
2024-08-18 18:33:10 +0530

    RestController

--- webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
+++ webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
@@ -1,13 +1,11 @@
 package com.springboot.webApp.Demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoClassController {
 
-	@ResponseBody
 	@RequestMapping("hello")
 	public String Hello() {
 		return "Hello, What are you learning??";
