d5f8b0bac5a15b538d2461a050a221321802d6d4
omjee247 <omjee247@gmail.com>
2024-08-22 08:31:42 +0530

    html response

--- webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
+++ webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
@@ -10,4 +10,18 @@ public class DemoClassController {
 	public String Hello() {
 		return "Hello, What are you learning??";
 	}
+	
+	@RequestMapping("say-hello-html")
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My first HTML Page</title>");
+		sb.append("<body>");
+		sb.append("My first html page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
+	
 }
