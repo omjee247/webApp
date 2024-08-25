bf2c3d6800d6e95dc9789790dbd8421a13de28ef
omjee247 <omjee247@gmail.com>
2024-08-25 17:09:42 +0530

    JSP ->Controller,responseBody,View

--- webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
+++ webApp/src/main/java/com/springboot/webApp/Demo/DemoClassController.java
@@ -1,17 +1,21 @@
 package com.springboot.webApp.Demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoClassController {
 
 	@RequestMapping("hello")
+	@ResponseBody
 	public String Hello() {
 		return "Hello, What are you learning??";
 	}
 	
 	@RequestMapping("say-hello-html")
+	@ResponseBody
 	public String sayHelloHtml() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("<html>");
@@ -23,5 +27,11 @@ public class DemoClassController {
 		sb.append("</html>");
 		return sb.toString();
 	}
+	
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJSP() {
+		return "sayHello";
+	}
+	
 	
 }
