2404ad2e60e8d00cc081d244e1cb8abf2c52ffad
omjee247 <omjee247@gmail.com>
2024-08-29 08:32:50 +0530

    Logging

--- webApp/src/main/java/com/springboot/webApp/Demo/login/LoginController/LoginController.java
+++ webApp/src/main/java/com/springboot/webApp/Demo/login/LoginController/LoginController.java
@@ -1,16 +1,27 @@
 package com.springboot.webApp.Demo.login.LoginController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class LoginController {
 
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("Login-JSP")
 	public String LoginJSP(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		
+		logger.debug("Request param is {}",name);
+		logger.info("I want this printed at info level");
+		logger.warn("I want this printed at warn level");
+		
 		return "login";
 	}
 }
